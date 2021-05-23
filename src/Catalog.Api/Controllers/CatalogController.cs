1d849084ac1524d14dbb22f756639649c6b206ed
Tom Fletcher <tom@tomfletcher.tech>
2021-05-23 20:07:50 +0100

    Registered Interfaces in Startup & Crud Actions in API

---
+++ src/Catalog.Api/Controllers/CatalogController.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Catalog.Api.Entities;
+using Catalog.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository productRepository, ILogger<CatalogController> logger)
+        {
+            _productRepository = productRepository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts() =>
+            Ok(await _productRepository.GetProducts());
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType((int) HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Product), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            Product product = await _productRepository.GetProduct(id);
+            if (product == null)
+            {
+                _logger.LogError($"Product with id: {id}, not found.");
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [Route("[action]/{category}", Name = "GetProductByCategory")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category) =>
+            Ok(await _productRepository.GetProductByCategory(category));
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _productRepository.CreateProduct(product);
+
+            return CreatedAtRoute("GetProduct", new {id = product.Id}, product);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int) HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product) =>
+            // ReSharper disable once HeapView.BoxingAllocation
+            Ok(await _productRepository.UpdateProduct(product));
+
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(Product), (int) HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteProductById(string id) =>
+            // ReSharper disable once HeapView.BoxingAllocation
+            Ok(await _productRepository.DeleteProduct(id));
+    }
+}
--- src/Catalog.Api/Startup.cs
+++ src/Catalog.Api/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Catalog.Api.Data;
+using Catalog.Api.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Catalog.Api
@@ -28,6 +23,9 @@ namespace Catalog.Api
         {
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Catalog.Api", Version = "v1"}); });
+
+            services.AddScoped<ICatalogContext, CatalogContext>();
+            services.AddScoped<IProductRepository, ProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
