03bb4d8d0459069d5b9f780ba73bc75410a0767c
Tom Fletcher <tom@tomfletcher.tech>
2021-10-27 06:50:42 +0100

    Added Domain objects.

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="N/A." file="EntityBase.cs">
+// </copyright>
+// <author>
+// Thomas Fletcher, Average Developer
+// tom@tomfletcher.tech
+// </author>
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="N/A." file="Order.cs">
+// </copyright>
+// <author>
+// Thomas Fletcher, Average Developer
+// tom@tomfletcher.tech
+// </author>
+// -----------------------------------------------------------------------
+
+using Ordering.Domain.Common;
+
+namespace Ordering.Domain.Entities
+{
+    public class Order : EntityBase
+    {
+        public string UserName { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string AddressLine { get; set; }
+
+        public string Country { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string CardName { get; set; }
+
+        public string CardNumber { get; set; }
+
+        public string Expiration { get; set; }
+
+        public string CVV { get; set; }
+
+        public int PaymentMethod { get; set; }
+    }
+}
