d92d311c4710e65e7aaf915885f2d5eaf9f48947
Tom Fletcher <tom@tomfletcher.tech>
2021-10-27 07:01:09 +0100

    Added Email Models.

---
+++ src/Services/Ordering/Ordering.Application/Models/Email.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="N/A." file="Email.cs">
+// </copyright>
+// <author>
+// Thomas Fletcher, Average Developer
+// tom@tomfletcher.tech
+// </author>
+// -----------------------------------------------------------------------
+
+namespace Ordering.Application.Models
+{
+    public class Email
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; } 
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Models/EmailSettings.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="N/A." file="EmailSettings.cs">
+// </copyright>
+// <author>
+// Thomas Fletcher, Average Developer
+// tom@tomfletcher.tech
+// </author>
+// -----------------------------------------------------------------------
+
+namespace Ordering.Application.Models
+{
+    public class EmailSettings
+    {
+        public string ApiKey { get; set; }
+        public string FromAddress { get; set; }
+        public string FromName { get; set; }
+    }
+}
