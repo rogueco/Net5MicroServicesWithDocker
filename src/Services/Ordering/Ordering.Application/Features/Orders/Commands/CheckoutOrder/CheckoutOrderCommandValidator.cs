42addae7354b083aa5f6759f02dc40a6e0b57b03
Tom Fletcher <tom@tomfletcher.tech>
2021-10-27 21:58:13 +0100

    Added Command validator

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="N/A." file="CheckoutOrderCommandValidator.cs">
+// </copyright>
+// <author>
+// Thomas Fletcher, Average Developer
+// tom@tomfletcher.tech
+// </author>
+// -----------------------------------------------------------------------
+
+using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty().WithMessage("{UserName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters.");
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} is required.");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is required.")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero.");
+        }
+    }
+}
