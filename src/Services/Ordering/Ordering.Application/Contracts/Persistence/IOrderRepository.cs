af98fcafc33a3a21956a70745abe0322293e6a23
Tom Fletcher <tom@tomfletcher.tech>
2021-10-27 06:59:29 +0100

    Added folders + interfaces for repositories.

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="N/A." file="IOrderRepository.cs">
+// </copyright>
+// <author>
+// Thomas Fletcher, Average Developer
+// tom@tomfletcher.tech
+// </author>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
