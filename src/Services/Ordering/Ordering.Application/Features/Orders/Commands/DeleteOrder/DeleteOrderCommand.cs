7e10e0ae4465ab3ba7124f5799c4162a14a49384
Tom Fletcher <tom@tomfletcher.tech>
2021-10-28 20:48:45 +0100

    Implemented Delete Command Handler

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="N/A." file="DeleteOrderCommand.cs">
+// </copyright>
+// <author>
+// Thomas Fletcher, Average Developer
+// tom@tomfletcher.tech
+// </author>
+// -----------------------------------------------------------------------
+
+using MediatR;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
