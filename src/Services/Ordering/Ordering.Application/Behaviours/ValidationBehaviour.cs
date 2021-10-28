0a86812c46d2d7f25e3020ea27e6fbc27f60c039
Tom Fletcher <tom@tomfletcher.tech>
2021-10-28 20:58:15 +0100

    Added Validation Behaviours.

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,47 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="N/A." file="ValidationBehaviour.cs">
+// </copyright>
+// <author>
+// Thomas Fletcher, Average Developer
+// tom@tomfletcher.tech
+// </author>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+
+namespace Ordering.Application.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                ValidationContext<TRequest> context = new ValidationContext<TRequest>(request);
+
+                ValidationResult[] validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                List<ValidationFailure> failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Count != 0)
+                    throw new ValidationException(failures);
+            }
+
+            return await next();
+        }
+    }
+}
