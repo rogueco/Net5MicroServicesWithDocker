5942bab89dccfd972dfecc0cb3324ccf777b65b5
Tom Fletcher <tom@tomfletcher.tech>
2021-10-30 14:10:40 +0100

    Added AutoMapper Profiles.

---
+++ src/Services/Basket/Basket.Api/Mapper/BasketProfile.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="N/A." file="BasketProfile.cs">
+// </copyright>
+// <author>
+// Thomas Fletcher, Average Developer
+// tom@tomfletcher.tech
+// </author>
+// -----------------------------------------------------------------------
+
+using AutoMapper;
+using Basket.Api.Entities;
+using EventBus.Messages.Events;
+
+namespace Basket.Api.Mapper
+{
+    public class BasketProfile : Profile
+    {
+        public BasketProfile()
+        {
+            CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
