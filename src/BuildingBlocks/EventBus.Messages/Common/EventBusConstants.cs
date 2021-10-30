ab300133c56b84ee1c88278715643684ef355c88
Tom Fletcher <tom@tomfletcher.tech>
2021-10-30 13:08:12 +0100

    Added EventBus class library.

---
+++ src/BuildingBlocks/EventBus.Messages/Common/EventBusConstants.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="N/A." file="EventBusConstants.cs">
+// </copyright>
+// <author>
+// Thomas Fletcher, Average Developer
+// tom@tomfletcher.tech
+// </author>
+// -----------------------------------------------------------------------
+
+namespace EventBus.Messages.Common
+{
+    public static class EventBusConstants
+    {
+        public const string BasketCheckoutQueue = "basketcheckout-queue";
+    }
+}
