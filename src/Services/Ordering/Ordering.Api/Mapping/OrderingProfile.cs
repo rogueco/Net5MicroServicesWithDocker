0b433dc98ef02e6df9054148e1955418b1cb4b97
Tom Fletcher <tom@tomfletcher.tech>
2021-11-02 07:51:12 +0000

    Added Automapper + Profile

---
+++ src/Services/Ordering/Ordering.Api/Mapping/OrderingProfile.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="N/A." file="OrderingProfile.cs">
+// </copyright>
+// <author>
+// Thomas Fletcher, Average Developer
+// tom@tomfletcher.tech
+// </author>
+// -----------------------------------------------------------------------
+
+using AutoMapper;
+using EventBus.Messages.Events;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+
+namespace Ordering.Api.Mapping
+{
+    public class OrderingProfile : Profile
+    {
+        public OrderingProfile()
+        {
+            CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
