e1d360ec62f7aca4baf508202b5c488be0bd2931
Tom Fletcher <tom@tomfletcher.tech>
2021-11-02 08:08:04 +0000

    Setup Message consumer

--- src/Services/Ordering/Ordering.Api/EventBusConsumer/BasketCheckoutConsumer.cs
+++ src/Services/Ordering/Ordering.Api/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -8,16 +8,35 @@
 // -----------------------------------------------------------------------
 
 using System.Threading.Tasks;
+using AutoMapper;
 using EventBus.Messages.Events;
 using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 
 namespace Ordering.Api.EventBusConsumer
 {
     public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
     {
-        public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+        private readonly ILogger<BasketCheckoutConsumer> _logger;
+
+        public BasketCheckoutConsumer(IMapper mapper, IMediator mediator, ILogger<BasketCheckoutConsumer> logger)
         {
-            throw new System.NotImplementedException();
+            _mapper = mapper;
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            CheckoutOrderCommand command = _mapper.Map<CheckoutOrderCommand>(context.Message);
+
+            int result = await _mediator.Send(command);
+
+            _logger.LogInformation("BasketCheckoutEvent consumed successfully. Created Order Id : {newOrderId}", result);
         }
     }
 }
