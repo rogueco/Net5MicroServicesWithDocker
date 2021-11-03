ea59f50eb408e9b7c07b1a4e556eddd8768ae19b
Tom Fletcher <tom@tomfletcher.tech>
2021-11-03 07:58:37 +0000

    Added Id to Orders DTO.

--- src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/OrdersVm.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/OrdersVm.cs
@@ -11,6 +11,8 @@ namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
 {
     public class OrdersVm
     {
+        public int Id { get; set; }
+        
         public string UserName { get; set; }
 
         public decimal TotalPrice { get; set; }
