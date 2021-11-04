396eefd70f5f0721f96fd23151251a2faed73663
Tom Fletcher <tom@tomfletcher.tech>
2021-11-04 07:57:59 +0000

    Implemented Catalog Service.

--- src/ApiGateways/Shopping.Aggregator/Services/Implementations/CatalogService.cs
+++ src/ApiGateways/Shopping.Aggregator/Services/Implementations/CatalogService.cs
@@ -1,7 +1,36 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services.Interfaces;
+
 namespace Shopping.Aggregator.Services.Implementations
 {
-    public class CatalogService
+    public class CatalogService : ICatalogService
     {
-        
+        private readonly HttpClient _httpClient;
+
+        public CatalogService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IEnumerable<CatalogModel>> GetCatalog()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("/api/v1/Catalog");
+            return await response.ReadContentAs<List<CatalogModel>>();
+        }
+
+        public async Task<CatalogModel> GetCatalog(string id)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"/api/v1/Catalog/{id}");
+            return await response.ReadContentAs<CatalogModel>();
+        }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        {
+            var response = await _httpClient.GetAsync($"/api/v1/Catalog/GetProductByCategory/{category}");
+            return await response.ReadContentAs<List<CatalogModel>>();
+        }
     }
 }
