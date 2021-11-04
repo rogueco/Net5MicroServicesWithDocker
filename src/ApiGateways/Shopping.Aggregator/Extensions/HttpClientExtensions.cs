d8606bcb6d01cbcda176932d7ccffc4bff59f202
Tom Fletcher <tom@tomfletcher.tech>
2021-11-04 08:05:40 +0000

    Code tidy.

--- src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -10,7 +10,9 @@ namespace Shopping.Aggregator.Extensions
         public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
         {
             if (!response.IsSuccessStatusCode)
+            {
                 throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
+            }
 
             string dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
 
