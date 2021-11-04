3ab3f75166f2aefd9dea7407951bc4db4edbbd0d
Tom Fletcher <tom@tomfletcher.tech>
2021-11-04 08:16:53 +0000

    Implemented Order Service.

--- src/ApiGateways/Shopping.Aggregator/Services/Implementations/OrderService.cs
+++ src/ApiGateways/Shopping.Aggregator/Services/Implementations/OrderService.cs
@@ -1,7 +1,25 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services.Interfaces;
+
 namespace Shopping.Aggregator.Services.Implementations
 {
-    public class OrderService
+    public class OrderService : IOrderService
     {
-        
+        private readonly HttpClient _httpClient;
+
+        public OrderService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrderByUserName(string userName)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"/api/v1/Order/{userName}");
+            return await response.ReadContentAs<List<OrderResponseModel>>();
+        }
     }
 }
