ab6ac0bee921ee11b9f1526ac18b5b4739cbb120
Tom Fletcher <tom@tomfletcher.tech>
2021-11-25 13:49:48 +0000

    Removed usings

--- src/WebApps/AspnetRunBasics/Startup.cs
+++ src/WebApps/AspnetRunBasics/Startup.cs
@@ -1,10 +1,7 @@
 using System;
-using AspnetRunBasics.Data;
-using AspnetRunBasics.Repositories;
 using AspnetRunBasics.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
